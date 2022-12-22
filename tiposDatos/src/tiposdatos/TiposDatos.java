9b83e999e3306b7bc400a81f4b33b1f60a961f41
Neydi <LE17081469@merida.tecnm.mx>
2022-12-21 19:49:58 -0600

    programa datos

---
+++ tiposDatos/src/tiposdatos/TiposDatos.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tiposdatos;
+
+/**
+ *
+ * @author NEYDI
+ */
+public class TiposDatos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // entre -128 a 127
+        byte enteroByte = 127;
+// entre -32768 a 32767
+        short enteroShort = 32767;
+// entre -2147483648 a 2147483647
+        int enteroInt = 2147483647;
+// entre -9223372036854775808 a 9223372036854775807
+        long enteroLong = 9223372036854775807L;
+        // entre -1.4E-45 a 3.4028235E38
+    float realFloat = 3.1416f;
+// entre -4.9E-324 a 1.7976931348623157E308
+        double realDouble = 4.7029235E3;
+        boolean desicion =false;
+        boolean desicion2=true;
+    }
+    
+}
